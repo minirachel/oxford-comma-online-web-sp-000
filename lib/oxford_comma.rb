5d1064b24b9cf64798d281fbb0a0d988dc4ed268
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-22 23:01:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size == 1
     array.join
-  if array.size == 2
+  elsif array.size == 2
     array.join(" and ")
   elsif array.size >= 3
     array_last = array.pop
