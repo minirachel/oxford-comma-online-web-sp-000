10b1c2c068a19edafc19c7e554b279c54fdca9ee
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-22 22:53:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
   array_last = array.pop
-  new_array = array.join(", ") << "and #{array_last}"
+  new_array = array.join(", ") << " and #{array_last}"
   new_array
+
+  if array.size == 1
+    LEAVE AS IS
+  if array.size == 2
+    array.join(" and ")
 end
