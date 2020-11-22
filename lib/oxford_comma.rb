944581359d7c23c0f45be07e767d4c3988d71059
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-22 23:01:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    array_last = array.pop
+    new_array = array.join(", ") << ", and #{array_last}"
+    new_array
+  end
 end
