8ee55a1edd941dd8b0f5832d71f4dca1f3c62a31
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-22 22:49:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  shortened_array = array.pop
-  shortened_array.join(", ")
-  new_array = shortened_array << "and #{array.last}"
+  array_last = array.pop
+  new_array = array.join(", ") << " and #{array_last}"
+  new_array
 end
